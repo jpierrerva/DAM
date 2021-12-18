2567c8811c30427c4273132a5bc7680d69ea4013
jpierrerva <j.pierre.rva@gmail.com>
2021-12-18 00:07:29 -0500

    bindingRegistro

--- app/src/main/java/com/example/petexpress/LoginActivity.kt
+++ app/src/main/java/com/example/petexpress/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.example.petexpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.petexpress.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        println("Holaaaaaaaaaaaaaaaaa")
+        val binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvRegistrate.setOnClickListener{
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+        }
 
 
     }
