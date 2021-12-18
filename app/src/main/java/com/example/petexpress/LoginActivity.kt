c37ccb0a6d935b541426f6098f4eef0dbcc5cbf4
jpierrerva <j.pierre.rva@gmail.com>
2021-12-17 23:45:28 -0500

    prueba

--- app/src/main/java/com/example/petexpress/LoginActivity.kt
+++ app/src/main/java/com/example/petexpress/LoginActivity.kt
@@ -7,5 +7,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        println("Holaaaaaaaaaaaaaaaaa")
+
+
     }
 }
