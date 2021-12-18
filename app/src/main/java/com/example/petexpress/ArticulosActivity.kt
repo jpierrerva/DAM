f9abef3bbd5e47aadbb7d53f79be0a157f3c456b
Vanessa Quispe <56798582+Vanesa8863@users.noreply.github.com>
2021-12-18 05:16:42 -0500

    Update ArticulosActivity.kt

--- app/src/main/java/com/example/petexpress/ArticulosActivity.kt
+++ app/src/main/java/com/example/petexpress/ArticulosActivity.kt
@@ -2,10 +2,18 @@ package com.example.petexpress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.petexpress.databinding.ActivityArticulosBinding
 
 class ArticulosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_articulos)
+        //setContentView(R.layout.activity_articulos)
+        val binding = ActivityArticulosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundleRecepcion = intent.extras
+
+        val animal = bundleRecepcion!!.getString("key_animal")
+
     }
 }
