4577152d87c1acb9627ccb392136980c2ed5a184
jpierrerva <j.pierre.rva@gmail.com>
2021-12-18 07:31:17 -0500

    login

--- app/src/main/java/com/example/petexpress/LoginActivity.kt
+++ app/src/main/java/com/example/petexpress/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.petexpress
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.petexpress.databinding.ActivityLoginBinding
 import com.example.petexpress.databinding.ActivityRegistroBinding
@@ -29,15 +30,14 @@ class LoginActivity : AppCompatActivity() {
 
                 Toast.makeText(this, "Debe ingresar correo", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
-            }
-            if (contra.isEmpty()){
+            }else if (contra.isEmpty()){
 
                 Toast.makeText(this, "Debe ingresar contrasenia", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
+            } else {
+                verificarLogin(correo, contra)
             }
 
-            val intent = Intent(this, SeleccionMascotasActivity::class.java)
-            startActivity(intent)
         }
 
         binding.tvRegistrate.setOnClickListener{
@@ -47,4 +47,21 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+
+    private fun verificarLogin(correo : String, contrasenia : String){
+        auth.signInWithEmailAndPassword(correo, contrasenia)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, SeleccionMascotasActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
